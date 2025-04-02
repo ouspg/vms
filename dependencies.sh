0bff5aa5cbf17b9efc30fce7cf56b4c9a910bd90
user65434680 <s2kava01@students.osao.fi>
2025-04-02 09:53:15 +0300

    updated dependecies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -12,6 +12,12 @@ if [ -f /etc/arch-release ]; then
 
 elif [ -f /etc/lsb-release ] || [ -f /etc/debian_version ]; then
     # Ubuntu (or Debian-based) commands
+    sudo apt update && sudo apt install -y wget gpg
+    sudo add-apt-repository universe
+    wget -O- https://apt.releases.hashicorp.com/gpg | sudo tee /etc/apt/trusted.gpg.d/hashicorp.asc
+    echo "deb [signed-by=/etc/apt/trusted.gpg.d/hashicorp.asc] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    sudo add-apt-repository universe -y
+
     sudo apt update
     sudo apt install -y packer python3 python3-pip python3-passlib ansible -y
     ansible-galaxy collection install community.general
