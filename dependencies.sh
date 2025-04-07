4f2eb47d6207a55f1d446e8eccd33e285c418e7a
user65434680 <s2kava01@students.osao.fi>
2025-04-07 14:43:14 +0300

    untested version for installation

--- dependencies.sh
+++ dependencies.sh
@@ -1,37 +1,31 @@
-if [ -f /etc/arch-release ]; then
-    # Arch Linux commands
-    sudo pacman -S packer --noconfirm
-    sudo pacman -S python3 --noconfirm
-    sudo pacman -S python3-pip --noconfirm
-    sudo pacman -S passlib --noconfirm
-    sudo pacman -S ansible --noconfirm
-    ansible-galaxy collection install community.general
-    
-    packer init packer_phase1.pkr.hcl
-    packer init packer_phase1.pkr.hcl
+#!/bin/bash
 
 
- 
-elif [ -f /etc/lsb-release ] || [ -f /etc/debian_version ]; then
- 
-    # Ubuntu (or Debian-based) commands
+echo "installing required packages please wait for install to finish. MANUAL CONFIG REGUIRED"
+read -p "Enter username for used for allas:" username
 
-    sudo apt update && sudo apt install -y wget gpg
-    sudo add-apt-repository universe
-    wget -O- https://apt.releases.hashicorp.com/gpg | sudo tee /etc/apt/trusted.gpg.d/hashicorp.asc
-    echo "deb [signed-by=/etc/apt/trusted.gpg.d/hashicorp.asc] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-    sudo add-apt-repository universe -y
-    sudo apt update
+sudo apt update && sudo apt install -y wget gpg
+sudo add-apt-repository universe
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo tee /etc/apt/trusted.gpg.d/hashicorp.asc
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/hashicorp.asc] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo add-apt-repository universe -y
+sudo apt update
  
-    sudo apt install -y packer python3 python3-pip python3-passlib ansible -y
+sudo apt install -y packer python3 python3-pip python3-passlib ansible -y
  
-    ansible-galaxy collection install community.general
+ansible-galaxy collection install community.general
  
-    sudo apt-get install qemu-system
+sudo apt-get install qemu-system
 
-    packer init packer_phase1.pkr.hcl
-    packer init packer_phase1.pkr.hcl
+packer init packer_phase1.pkr.hcl
+packer init packer_phase1.pkr.hcl
 
-else
-    echo "Unsupported OS"
-fi
+
+sudo pip3 install python-openstackclient
+sudo apt install restic
+curl https://rclone.org/install.sh | sudo bash
+sudo pip3 install s3cmd
+
+wget https://raw.githubusercontent.com/CSCfi/allas-cli-utils/master/allas_conf
+
+source allas_conf --mode S3 --user ${username}
