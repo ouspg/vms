224fbb2ca5ac52ec282c7df38a93ba0102485fc2
user65434680 <s2kava01@students.osao.fi>
2025-04-07 11:59:06 +0300

    untested version for installation

--- dependencies.sh
+++ dependencies.sh
@@ -10,21 +10,23 @@ if [ -f /etc/arch-release ]; then
     packer init packer_phase1.pkr.hcl
     packer init packer_phase1.pkr.hcl
 
+
+ 
 elif [ -f /etc/lsb-release ] || [ -f /etc/debian_version ]; then
+ 
     # Ubuntu (or Debian-based) commands
+
     sudo apt update && sudo apt install -y wget gpg
     sudo add-apt-repository universe
     wget -O- https://apt.releases.hashicorp.com/gpg | sudo tee /etc/apt/trusted.gpg.d/hashicorp.asc
-    echo "deb [signed-by=/etc/apt/trusted.gpg.d/hashicorp.asc] https://apt.releases.hashicorp.com $(lsb_>
+    echo "deb [signed-by=/etc/apt/trusted.gpg.d/hashicorp.asc] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
     sudo add-apt-repository universe -y
-
     sudo apt update
-    sudo apt install ovmf -y
-    sudo apt install -y packer python3 python3-pip python3-passlib ansible
-    pip install --upgrade ansible
+ 
+    sudo apt install -y packer python3 python3-pip python3-passlib ansible -y
+ 
     ansible-galaxy collection install community.general
  
-    
     sudo apt-get install qemu-system
 
     packer init packer_phase1.pkr.hcl
